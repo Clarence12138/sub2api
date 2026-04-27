40ac54cfd233f943c40ef88daac70d81b6d2c328
Clarence <1297529513@qq.com>
2026-04-27 21:22:28 +0800

    chore(deploy): 默认使用 fork 仓库和 GHCR 镜像

--- deploy/docker-deploy.sh
+++ deploy/docker-deploy.sh
@@ -20,8 +20,10 @@ YELLOW='\033[1;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
-# GitHub raw content base URL
-GITHUB_RAW_URL="https://raw.githubusercontent.com/Wei-Shaw/sub2api/main/deploy"
+# GitHub source for deployment files (override when needed)
+GITHUB_REPO="${GITHUB_REPO:-Clarence12138/sub2api}"
+GITHUB_REF="${GITHUB_REF:-main}"
+GITHUB_RAW_URL="https://raw.githubusercontent.com/${GITHUB_REPO}/${GITHUB_REF}/deploy"
 
 # Print colored message
 print_info() {
